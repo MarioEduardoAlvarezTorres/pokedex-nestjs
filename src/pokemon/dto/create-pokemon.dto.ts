ea1ab91f3fc9efba46a3bc4a73308dd92b14296b
MarioEduardo <eduardoat44@gmail.com>
2023-01-10 15:32:50 -0600

    Recibir y validar la dara

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePokemonDto {}
+import { IsPositive, IsString, Min, MinLength, IsInt } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
